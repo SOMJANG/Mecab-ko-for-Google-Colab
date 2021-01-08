bac9777dd93cbdf7a4c3734b4cf532baba8b2ea3
Hyunseung Jeon <dogdriip@gmail.com>
2021-01-08 19:22:47 +0900

    Add light version

---
+++ install_mecab-ko_on_colab_light_210108.sh
@@ -0,0 +1,76 @@
+
+# 
+
+cd /content
+
+echo "Installing konlpy....."
+! pip3 install konlpy
+echo "Done"
+
+
+echo "Installing mecab-0.996-ko-0.9.2.tar.gz....."
+
+echo "Downloading mecab-0.996-ko-0.9.2.tar.gz......."
+echo "from https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz"
+! wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz 
+echo "Done"
+
+echo "Unpacking mecab-0.996-ko-0.9.2.tar.gz......."
+! tar xvfz mecab-0.996-ko-0.9.2.tar.gz > /dev/null 2>&1 
+echo "Done"
+
+echo "Change Directory to mecab-0.996-ko-0.9.2......."
+cd mecab-0.996-ko-0.9.2/
+
+echo "installing mecab-0.996-ko-0.9.2.tar.gz........"
+echo 'configure'
+! ./configure > /dev/null 2>&1
+echo 'make'
+! make > /dev/null 2>&1
+echo 'make check'
+! make check > /dev/null 2>&1
+echo 'make install'
+! make install > /dev/null 2>&1
+
+echo 'ldconfig'
+! ldconfig > /dev/null 2>&1
+echo "Done"
+
+echo "Change Directory to /content"
+cd ../
+
+echo "Downloading mecab-ko-dic-2.1.1-20180720.tar.gz......."
+echo "from https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz"
+! wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
+echo "Done"
+ 
+echo "Unpacking  mecab-ko-dic-2.1.1-20180720.tar.gz......."
+! tar xvfz mecab-ko-dic-2.1.1-20180720.tar.gz > /dev/null 2>&1
+echo "Done"
+
+echo "Change Directory to mecab-ko-dic-2.1.1-20180720"
+cd mecab-ko-dic-2.1.1-20180720/
+echo "Done"
+
+echo "installing........"
+echo 'configure'
+! ./configure > /dev/null 2>&1
+echo 'make'
+! make > /dev/null 2>&1
+echo 'make install'
+! make install > /dev/null 2>&1
+
+echo 'bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)'
+! bash <(curl -s https://raw.githubusercontent.com/konlpy/konlpy/master/scripts/mecab.sh)  > /dev/null 2>&1
+echo "Done"
+
+echo "Successfully Installed"
+
+echo "Now you can use Mecab"
+echo "from konlpy.tag import Mecab"
+echo "mecab = Mecab()"
+
+echo "사용자 사전 추가 방법 : https://bit.ly/3k0ZH53"
+
+echo "NameError: name 'Tagger' is not defined 오류 발생 시 런타임을 재실행 해주세요"
+echo "블로그에 해결 방법을 남겨주신 tana님 감사합니다."
